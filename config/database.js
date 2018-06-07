e13e77441b3a3c8410a523873de329cf049107c2
saiakhil25 <saiakhil@shootup.in>
2018-06-07 20:22:54 +0530

    setup

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
   // local
-  url: 'mongodb://localhost:27017/heroku_fd9h8h47'
+  //url: 'mongodb://localhost:27017/heroku_fd9h8h47'
   // production
+  url: 'mongodb://flatman:Thisisakhi@97@ds151530.mlab.com:51530/flatman'
   //'url':'https://gentle-depths-76096.herokuapp.com/'
   //'url': 'mongodb://heroku_fd9h8h47:dq9ofuj060ipaeo6vce87k6860@ds161459.mlab.com:61459/heroku_fd9h8h47'
 };
